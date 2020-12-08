ce2cc8a56086fcd9ded2be2271b70e0fd086a2f7
lay <2491812016@qq.com>
2020-12-08 17:54:34 +0800

    新增

--- app/src/main/java/com/example/activitypro/MainActivity.kt
+++ app/src/main/java/com/example/activitypro/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.activitypro
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.activitypro.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val TEXT_VIEW_CONTENT = "textContent"
+    private lateinit var mainBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
+
+    }
+
+    //恢复
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        mainBinding.tvContent.text = savedInstanceState.getString(TEXT_VIEW_CONTENT)
+    }
+
+    //保存
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.run {
+            putString(TEXT_VIEW_CONTENT,mainBinding.tvContent.text.toString())
+        }
     }
 }
